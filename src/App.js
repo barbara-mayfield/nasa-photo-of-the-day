f534d09660d311ef0e79c6cb38bb728a91bb6426
Barbara Mayfield <bmayfield1991@gmail.com>
2019-08-28 17:04:28 -0500

    changed state from title to data to make code more DRY, fetched data from the API and ready to style page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import SpaceImage from './components/DailyImage/SpaceImage'
 import "./App.css";
 
 function App() {
-  const [title, setTitle] = useState('');
-  console.log("top");
+  const [data, setData] = useState({});
+
   axios.get('https://api.nasa.gov/planetary/apod?api_key=EftiSMpbnRmD444w7EM7AK0KacuxQicHZ53m8BkF')
     .then(res => {
       console.log(res.data);
-      setTitle(res.data.title);
+      setData(res.data);
     })
-  console.log("bottom");
 
   return (
     <div className="App">
@@ -19,9 +18,9 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
-      <SpaceImage title="Props Title"
-                  url="Not a url lol"
-                  explanation="an explanation" />
+      <SpaceImage title={data.title}
+                  url={data.url}
+                  explanation={data.explanation} />
     </div>
   );
 }
