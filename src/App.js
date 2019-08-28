d6df902c8584daa2a15ddf82b684da18e2badced
Barbara Mayfield <bmayfield1991@gmail.com>
2019-08-28 17:58:06 -0500

    added useEffect to render the api data, removed axios import from SpaceImage since it's not using axios.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import SpaceImage from './components/DailyImage/SpaceImage'
 import "./App.css";
@@ -6,11 +6,15 @@ import "./App.css";
 function App() {
   const [data, setData] = useState({});
 
-  axios.get('https://api.nasa.gov/planetary/apod?api_key=EftiSMpbnRmD444w7EM7AK0KacuxQicHZ53m8BkF')
-    .then(res => {
-      console.log(res.data);
-      setData(res.data);
-    })
+  useEffect(() => {
+    console.log('first render')
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=EftiSMpbnRmD444w7EM7AK0KacuxQicHZ53m8BkF')
+      .then(res => {
+        console.log(res.data);
+        setData(res.data);
+      });
+  }, []);
+
 
   return (
     <div className="App">
